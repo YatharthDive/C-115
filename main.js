1b3cb869c24b720db33f02a5d908c66626f51c0f
YatharthDive <71880750+YatharthDive@users.noreply.github.com>
2021-06-04 15:37:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function preload(){
-
+beard=loadImage('https://i.postimg.cc/D0k2JVRZ/beard-clipart-5.png');
+wig=loadImage('https://i.postimg.cc/nz88T42S/Daco-389918.png');
 }
+noseX=0;
+noseY=0;
 function setup(){
     canvas=createCanvas(400,400);
     canvas.center();
@@ -10,13 +13,16 @@ function setup(){
     postnet=ml5.poseNet(cam,Yatharth);
     postnet.on('pose',getposes)
 }
+function Yatharth() {
+    console.log('PoseNet Is Initialized');
+}
 function getposes(results) {
     if (results.length>0) {
         console.log(results);
         console.log("nose x= "+results[0].pose.nose.x);
         console.log("nose y= "+results[0].pose.nose.y);
-        console.log("righteye x= "+results[0].pose.rightEye.x);
-        console.log("righteye y= "+results[0].pose.rightEye.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
     }
 }
 function Yatharth() {
@@ -24,6 +30,11 @@ function Yatharth() {
 }
 function draw(){
 image(cam,0,0,400,400);
+fill("Red");
+stroke("Blue")
+//circle(noseX,noseY,30);
+image(beard,noseX-45,noseY+7,90,90);
+image(wig,noseX-60,noseY-130,120,90);
 }
 function take_snapshot(){
     save("myfilter.image.png");
